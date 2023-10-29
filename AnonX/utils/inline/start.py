ad34f500f64253543af349c91e15b40991983e58
L2LUCKY <145443484+L2LUCKY@users.noreply.github.com>
2023-10-29 13:29:52 +0530

    Update start.py

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -9,7 +9,7 @@ def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text="ᴀᴅᴅ ᴅᴀʙᴀɴɢ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ",
+                text="ᴀᴅᴅ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ",
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         ],
@@ -30,7 +30,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text="❀ ᴀᴅᴅ ᴅᴀʙᴀɴɢ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ ❀",
+                text="❀ ᴀᴅᴅ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ ❀",
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         ],
